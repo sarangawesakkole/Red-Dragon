64b4cd10e644a7ac1b4eed48ca020a9697d05cca
sarangawesakkole <132681927+sarangawesakkole@users.noreply.github.com>
2023-05-06 04:30:05 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -29,14 +29,14 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
-global.ytname = "" // yt chanel name
-global.socialm = "GitHub: prabhasha2006" // github or insta name
-global.location = "India, Mizoram, Aizawl" // location
+global.vcardowner = ['+94 70 293 1586'] // owner number
+global.ownername = "Saranga Lakshitha" // owner name
+global.ytname = "Virindu LK" // yt chanel name
+global.socialm = "Youtube: https://youtube.com/@virindulk" // github or insta name
+global.location = "Sri lanka, Rathnapura, Ayagama" // location
 
 //bot body 
-global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
+global.alivepic = `https://telegra.ph/zxxff-05-05` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
 global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
@@ -47,16 +47,16 @@ global.upsongvoice = "false" // true/false \\ bot can upload song as voice note 
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
-global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.owner = ['+94702931586']
+global.ownertag = '+94702931586' //your tag number
+global.botname = '𝚅𝙰𝙼𝙿𝙸𝚁𝙴ᴮᴼᵀ' //ur bot name
+global.packname = "𝚅𝙰𝙼𝙿𝙸𝚁𝙴ᴮᴼᵀ Sticker" // sticker packname
+global.author = "𝚅𝙰𝙼𝙿𝙸𝚁𝙴ᴮᴼᵀ" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '[ 𝚅𝙰𝙼𝙿𝙸𝚁𝙴ᴮᴼᵀ ]' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+94702931586'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
